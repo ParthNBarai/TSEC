c5e06ad067cd2bd165fb2d2b7b014bf376e11e96
Parth Barai <parthnbarai1234@gmail.com>
2023-02-02 18:19:36 +0530

    owner and parent login done

--- Routes/owner.js
+++ Routes/owner.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = express.Router();
 const { body, check, validationResult } = require('express-validator');
 const fetchuser = require('../Middleware/fetchuser');
+const { genSaltSync, hashSync, compareSync } = require("bcrypt");
+const { sign, verify } = require("jsonwebtoken");
+const auth = require('../Authentication/GetBearerToken')
 const OwnerSchema = require('../Schemas/OwnerSchema');
 const multer = require('../Middleware/multer')
 require("dotenv/config");
@@ -16,6 +19,8 @@ router.post('/signup', multer.upload.single('image'), body('password', 'Password
                 res.status(401).json(valerr)
             }
             else {
+                const salt = genSaltSync(10);
+                req.body.password = hashSync(req.body.password, salt);
                 const newOwner = new OwnerSchema({
                     email: req.body.email,
                     password: req.body.password,
@@ -34,6 +39,74 @@ router.post('/signup', multer.upload.single('image'), body('password', 'Password
             console.log(err.message)
             res.status(500).json({ message: err.message });
 
+        }
+    })
+
+
+router.post('/login',
+    body('password', 'Password must be atleast 8 characters').isLength({ min: 8 }), async (req, res) => {
+        try {
+            var valerr = validationResult(req);
+            if (!valerr.isEmpty()) {
+                console.log(valerr.mapped())
+                res.status(401).json(valerr)
+            }
+            else {
+
+                const user = { phone: req.body.phone };
+                OwnerSchema.findOne({ phone: req.body.phone })
+                    .exec()
+                    .then(async user => {
+                        // console.log(user)
+                        if (!user) {
+                            return res.status(403).json({
+                                error: {
+                                    message: "User Not Found, Kindly Register!"
+                                }
+                            })
+                        }
+
+                        else {
+                            const result = compareSync(req.body.password, user.password);
+                            if (result) {
+                                user.password = undefined;
+                                const newUser = {
+                                    id: user.id,
+                                    phone: user.phone,
+                                    name: user.name,
+                                    device_id: user.device_id
+                                }
+                                // console.log(newUser)
+                                const jsontoken = await auth.tokenGenerate(req, res, newUser);
+
+                                // console.log("refresh")
+                                // console.log(refresh)
+
+                                return res.status(200).json({
+                                    success: 1,
+                                    message: "Successful login!",
+                                    token: jsontoken,
+
+                                });
+                            }
+                            else {
+                                // console.log(err.message)
+                                return res.status(403).json({
+                                    error: {
+                                        message: "Username or Password Invalid!"
+                                    }
+                                })
+                            }
+
+                        }
+
+                    })
+            }
+
+        }
+        catch (err) {
+            console.log(err.message)
+            res.status(500).json({ message: err.message });
         }
     })
 
--- Routes/parent.js
+++ Routes/parent.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = express.Router();
 const { body, check, validationResult } = require('express-validator');
 const fetchuser = require('../Middleware/fetchuser');
+const { genSaltSync, hashSync, compareSync } = require("bcrypt");
+const { sign, verify } = require("jsonwebtoken");
+const auth = require('../Authentication/GetBearerToken')
 const ParentSchema = require('../Schemas/ParentSchema');
 require("dotenv/config");
 
@@ -17,7 +20,8 @@ router.post('/signup', body('name', 'Enter a valid Name').isLength({ min: 3 }),
                 res.status(401).json(valerr)
             }
             else {
-
+                const salt = genSaltSync(10);
+                req.body.password = hashSync(req.body.password, salt);
                 const newParent = new ParentSchema({
                     email: req.body.email,
                     password: req.body.password,
@@ -38,6 +42,74 @@ router.post('/signup', body('name', 'Enter a valid Name').isLength({ min: 3 }),
             res.status(500).json({ message: err.message });
         }
 
+    })
+
+
+router.post('/login',
+    body('password', 'Password must be atleast 8 characters').isLength({ min: 8 }), async (req, res) => {
+        try {
+            var valerr = validationResult(req);
+            if (!valerr.isEmpty()) {
+                console.log(valerr.mapped())
+                res.status(401).json(valerr)
+            }
+            else {
+
+                const user = { phone: req.body.phone };
+                ParentSchema.findOne({ phone: req.body.phone })
+                    .exec()
+                    .then(async user => {
+                        // console.log(user)
+                        if (!user) {
+                            return res.status(403).json({
+                                error: {
+                                    message: "User Not Found, Kindly Register!"
+                                }
+                            })
+                        }
+
+                        else {
+                            const result = compareSync(req.body.password, user.password);
+                            if (result) {
+                                user.password = undefined;
+                                const newUser = {
+                                    id: user.id,
+                                    phone: user.phone,
+                                    name: user.name,
+                                    device_id: user.device_id
+                                }
+                                // console.log(newUser)
+                                const jsontoken = await auth.tokenGenerate(req, res, newUser);
+
+                                // console.log("refresh")
+                                // console.log(refresh)
+
+                                return res.status(200).json({
+                                    success: 1,
+                                    message: "Successful login!",
+                                    token: jsontoken,
+
+                                });
+                            }
+                            else {
+                                // console.log(err.message)
+                                return res.status(403).json({
+                                    error: {
+                                        message: "Username or Password Invalid!"
+                                    }
+                                })
+                            }
+
+                        }
+
+                    })
+            }
+
+        }
+        catch (err) {
+            console.log(err.message)
+            res.status(500).json({ message: err.message });
+        }
     })
 
 module.exports = router
